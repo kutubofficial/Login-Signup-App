171eb76df7f6c712da6040d1bf59658d401244e3
Kutubuddin Ansari <kutubofficial599@gmail.com>
2025-04-15 01:09:06 +0530

    added animation in profile component

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { motion } from "framer-motion";
 import style from "./profile.module.css";
+import Lottie from "lottie-react";
+import animationData from "./welcome-animation.json";
+import { TypeAnimation } from "react-type-animation";
 
 const Profile = () => {
   const [profileData, setProfileData] = useState(null);
@@ -27,11 +29,24 @@ const Profile = () => {
 
   return (
     <div id={style.container}>
-      <h1>
-        {" "}
-        Hey {profileData?.username} 👋 <br />
-        Welcome to your profile
-      </h1>
+      <TypeAnimation
+        sequence={[
+          `Welcome to your profile 👋`,
+          2000,
+          "Hope you have a great day!",
+          2000,
+        ]}
+        wrapper="span"
+        speed={50}
+        style={{ fontSize: "24px", display: "inline-block", marginTop: "20px" }}
+        repeat={Infinity}
+      />
+
+      <Lottie
+        animationData={animationData}
+        loop={true}
+        className={style.lottieAnimation}
+      />
     </div>
   );
 };
